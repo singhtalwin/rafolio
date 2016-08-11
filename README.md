5d9b254d0f9ac76c479a8140173a317b78754136
Talwinder Singh <singhthing@gmail.com>
2016-08-11 00:50:07 -0700

    Express-Setup express and npm setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rafolio
 Raf Tan Portfolio
+
+
+# NPM Instructions
+`npm run dev` for development
+`npm start` for production
---
+++ src/server.js
@@ -0,0 +1,24 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// set the views dir
+app.set('views', 'src/views/');
+
+// set the public root
+app.use('/public', express.static('public'));
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(port);
+console.log(`Server listing on port ${port}`);
