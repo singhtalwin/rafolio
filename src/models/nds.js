87a5c54acd1c60d2946816b1e6d36f0df34a72be
Talwinder Singh <singhthing@gmail.com>
2016-10-02 19:18:46 -0700

    NDS-Page page, styles added, refactored landing page styles

---
+++ src/models/nds.js
@@ -0,0 +1,12 @@
+module.exports = {
+    credits: {
+        title: 'Credits',
+        items: [
+            { label: 'Agency', text: 'NURUN' },
+            { label: 'Art Direction', text: 'Christopher Grose' },
+            { label: 'UI Designer', text: 'Rafael Tan' },
+            { label: 'UX Designer', text: 'Ryan Eves' },
+            { label: 'Date', text: '2016' }
+        ]
+    }
+};
