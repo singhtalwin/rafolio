07457b2bd1746b80a2fae2b0a0502863950325cd
Talwinder Singht <singhtalwin@gmail.com>
2016-10-20 00:41:33 -0700

    [Routing] Refactor routing and page links

---
+++ src/models/routes.js
@@ -0,0 +1,26 @@
+const nds = require('./nds');
+const starWars = require('./star-wars');
+const zuru = require('./zuru');
+
+module.exports = {
+    landing: {
+        model: {},
+        path: '/',
+        view: 'pages/landing'
+    },
+    nds: {
+        model: nds,
+        path: '/nds',
+        view: 'pages/nds'
+    },
+    starWars: {
+        model: starWars,
+        path: '/star-wars',
+        view: 'pages/star-wars'
+    },
+    zuru: {
+        model: zuru,
+        path: '/zuru',
+        view: 'pages/zuru'
+    }
+};
