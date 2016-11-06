0d5b661e5dd6f7befca0775b3a0227c768c192fd
Talwinder Singht <singhtalwin@gmail.com>
2016-11-05 19:48:37 -0700

    [App-Compression] Use compression for express app

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 // server.js
+const compression = require('compression');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
@@ -10,16 +11,19 @@ app.set('view engine', 'ejs');
 // set the views dir
 app.set('views', 'src/views/');
 
+// use compression
+app.use(compression());
+
 // set the public root
 app.use('/public', express.static('public'));
 app.use('/build', express.static('build'));
 
 Object.keys(routes).forEach((key) => {
     const route = routes[key];
-    
+
     app.get(route.path, (req, res) => {
         res.locals.routes = routes;
-        res.render(route.view, route.model);    
+        res.render(route.view, route.model);
     });
 });
 
